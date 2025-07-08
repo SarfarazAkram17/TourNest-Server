e5baf48a39dd5445ee17027803b894a7e9d73a13
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-07-08 14:57:02 +0600

    setup project

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send('Assignment 12 is cooking')
+})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.or0q8ig.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, ()=>{
+    console.log(`Assignment 12 running on port ${port}`)
+})
