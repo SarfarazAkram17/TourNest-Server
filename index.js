24751db69eebd3b0428d2e4817614846a72600ad
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-07-16 20:54:24 +0600

    Complete TourNest server side

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function run() {
   };
 
   try {
-    // await client.connect();
+    await client.connect();
 
     const db = client.db("TourNest");
     const usersCollection = db.collection("usersCollection");
@@ -874,7 +874,7 @@ async function run() {
       }
     });
 
-    // await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     console.log("Connected to MongoDB!");
   } finally {
   }
